aa70c0f23b535801f6c4e1fa6a30bda6ac756b6d
Robert Hoppe <rh@21sheeps.com>
2020-05-13 17:11:29 +0200

    Adjust further dealys

--- pkg/model/keycloak_deployment.go
+++ pkg/model/keycloak_deployment.go
@@ -14,8 +14,8 @@ import (
 )
 
 const (
-	LivenessProbeInitialDelay  = 40
-	ReadinessProbeInitialDelay = 50
+	LivenessProbeInitialDelay  = 150
+	ReadinessProbeInitialDelay = 160
 	//10s (curl) + 10s (curl) + 10s (just in case)
 	ProbeTimeoutSeconds         = 30
 	ProbeTimeBetweenRunsSeconds = 60
